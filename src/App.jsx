262f414c4dc8da1c43b50ca5179e1897a0ed5ed8
tong <lbuchiyu@gmail.com>
2023-02-08 23:57:07 +0800

    feat: 完成table展示

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,48 @@
-import React from 'react'
-import { createForm } from '@formily/core'
-import { FormProvider, FormConsumer, Field } from '@formily/react'
-import { FormItem, FormLayout, Input, FormButtonGroup, Submit } from '@formily/next'
-
-const form = createForm()
+import React, { useState, useEffect } from 'react'
+import { Table } from '@alifd/next'
 
 function App () {
+  const [dataSource, setDataSource] = useState([])
+  const [columns, setColumns] = useState([])
+  console.log('🚀~ 10 App columns', columns)
+
+  useEffect(() => {
+    const files = window.utools.getCopyedFiles()
+    if (Array.isArray(files) && files.length === 1 && files[0].isFile && files[0].path) {
+      import(/* @vite-ignore */`${files[0].path}?sheetjs-10`).then(({ default: data }) => {
+        setDataSource(data.map((item, index) => ({ ...item, index })))
+        if (data?.[0]) {
+          setColumns([{
+            title: '行号',
+            dataIndex: 'index',
+            width: 60,
+            lock: 'left'
+          }].concat(Object.keys(data?.[0] || []).map((title) => {
+            return {
+              title,
+              dataIndex: title,
+              width: 100,
+              asyncResizable: true
+            }
+          })))
+        }
+      })
+    }
+  }, [])
+
   return (
-    <FormProvider form={form}>
-      <FormLayout layout="vertical">
-        <Field
-          name="input"
-          title="输入框"
-          required
-          initialValue="Hello world"
-          decorator={[FormItem]}
-          component={[Input]}
-        />
-      </FormLayout>
-      <FormConsumer>
-        {() => (
-          <div
-            style={{
-              marginBottom: 20,
-              padding: 5,
-              border: '1px dashed #666'
-            }}
-          >
-            实时响应：{form.values.input}
-          </div>
-        )}
-      </FormConsumer>
-      <FormButtonGroup>
-        <Submit onSubmit={console.log}>提交</Submit>
-      </FormButtonGroup>
-    </FormProvider>
+    <Table
+      primaryKey="index"
+      size="small"
+      columns={columns}
+      dataSource={dataSource}
+      onResizeChange={(dataIndex, value) => {
+        const toChangeIndex = columns.findIndex(item => item.dataIndex === dataIndex)
+        columns[toChangeIndex].width = columns[toChangeIndex].width + value
+        setColumns(columns)
+      }}
+      emptyContent="暂无数据"
+    />
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import utools from 'vite-plugin-utools'
+import { read, utils } from 'xlsx'
+import { readFileSync } from 'fs'
+
+const sheetMather = /\?sheetjs-((\d+)|all)$/
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.xlsx'],
   resolve: {
     alias: [
       {
@@ -27,6 +32,21 @@ export default defineConfig({
         outDir: 'upx',
         outName: '[pluginName]_[version].upx'
       }
-    })
+    }),
+    {
+      name: 'vite-sheet',
+      transform (code, id) {
+        const reMatcher = id.match(sheetMather)
+        if (!reMatcher) return
+
+        const opts = {}
+        if (reMatcher[1] !== 'all') opts.sheetRows = Number(reMatcher[1])
+
+        const wb = read(readFileSync(id.replace(sheetMather, '')), opts)
+        const data = utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+
+        return `export default JSON.parse('${JSON.stringify(data)}')`
+      }
+    }
   ]
 })
